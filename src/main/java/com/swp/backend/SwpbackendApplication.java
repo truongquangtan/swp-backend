461f02e8cdf0149d0e963c0e7d02d7b386859279
Trung Minh <nguyenminhtrungse@gmail.com>
2022-05-09 03:31:46 +0700

    setup base include firebae, and mail service via stmp (cause: can't create account sendgrid)

---
+++ src/main/java/com/swp/backend/SwpbackendApplication.java
@@ -0,0 +1,13 @@
+package com.swp.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwpbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwpbackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/swp/backend/SwpbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swp.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwpbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
