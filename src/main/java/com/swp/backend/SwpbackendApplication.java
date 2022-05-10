d713f2b46bfe456d16016546e1550d274c504f28
truongquangtan <truongquangtan2001@gmail.com>
2022-05-10 23:03:14 +0700

    Add Database Config

--- src/main/java/com/swp/backend/SwpbackendApplication.java
+++ src/main/java/com/swp/backend/SwpbackendApplication.java
@@ -5,9 +5,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SwpbackendApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(SwpbackendApplication.class, args);
-    }
-
+    public static void main(String[] args) {SpringApplication.run(SwpbackendApplication.class, args);}
 }
