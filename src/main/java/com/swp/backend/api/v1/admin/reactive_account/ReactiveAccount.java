f72f5b9c5ac59cca5eaa121d82fe07bfb0da2a05
unknown <nguyenminhtrungse@gmail.com>
2022-06-10 07:38:59 +0700

    Fix bug set default is-active = true Add-Yard-API

---
+++ src/main/java/com/swp/backend/api/v1/admin/reactive_account/ReactiveAccount.java
@@ -0,0 +1,9 @@
+package com.swp.backend.api.v1.admin.reactive_account;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping(value = "api/v1/admin")
+@RestController
+public class ReactiveAccount {
+}
