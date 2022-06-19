6361ae70a1e369db587f172156505ebee3beb0bf
truongquangtan <truongquangtan2001@gmail.com>
2022-06-19 22:48:11 +0700

    Request get user modify.

--- src/main/java/com/swp/backend/api/v1/admin/modify_account/ModifyAccountRequest.java
+++ src/main/java/com/swp/backend/api/v1/admin/modify_account/ModifyAccountRequest.java
@@ -2,6 +2,7 @@ package com.swp.backend.api.v1.admin.modify_account;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.swp.backend.utils.RegexHelper;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,7 +13,6 @@ import org.springframework.boot.context.properties.bind.DefaultValue;
 @NoArgsConstructor
 @Data
 public class ModifyAccountRequest {
-    public static final String PHONE_REGEX = "\\d{10}";
     private String fullName;
     private String phone;
 
@@ -21,7 +21,7 @@ public class ModifyAccountRequest {
 
     public boolean isValid()
     {
-        if((fullName != null && fullName.trim().equals("")) || (phone != null && !phone.trim().equals("") && !phone.matches(PHONE_REGEX)))
+        if((fullName != null && fullName.trim().equals("")) || (phone != null && !phone.trim().equals("") && !phone.matches(RegexHelper.PHONE_REGEX_LOCAL)))
         {
             return false;
         }
