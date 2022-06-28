ccd5e7c34ac639d197471a88c18fd441974d19cf
truongquangtan <truongquangtan2001@gmail.com>
2022-06-28 12:00:10 +0700

    Order sub-yard in owner screen and fix delete subyard function.

--- src/main/java/com/swp/backend/constance/NoImageUrl.java
+++ src/main/java/com/swp/backend/constance/NoImageUrl.java
@@ -1,5 +1,5 @@
 package com.swp.backend.constance;
 
 public class NoImageUrl {
-    public static final String NO_IMAGE = "https://firebasestorage.googleapis.com/v0/b/fu-swp391.appspot.com/o/9044950_no_image_icon.png?alt=media";
+    public static final String NO_IMAGE = "https://firebasestorage.googleapis.com/v0/b/fu-swp391.appspot.com/o/NO_IMAGE.png?alt=media";
 }
