4720d7f8b5ec377461d40b3045a1df77f6e55019
truongquangtan <truongquangtan2001@gmail.com>
2022-07-07 23:13:41 +0700

    query err

--- src/main/java/com/swp/backend/myrepository/YardReportCustomRepository.java
+++ src/main/java/com/swp/backend/myrepository/YardReportCustomRepository.java
@@ -23,7 +23,7 @@ public class YardReportCustomRepository {
 
         try
         {
-            String nativeQuery = "SELECT yr.id, yr.user_id, yr.yard_id, y.owner_id, a.full_name, y.name, y.address, account_owner.email, yr.status, yr.created_at, yr.updated_at, yr.reason, yr.reference, a.email, account_owner.name" +
+            String nativeQuery = "SELECT yr.id, yr.user_id, yr.yard_id, y.owner_id, a.full_name, y.name, y.address, account_owner.email, yr.status, yr.created_at, yr.updated_at, yr.reason, yr.reference, a.email, account_owner.full_name" +
                     " FROM yard_report yr INNER JOIN yards y ON yr.yard_id = y.id" +
                     "                     INNER JOIN accounts a ON yr.user_id = a.id" +
                     "                     INNER JOIN accounts account_owner ON y.owner_id = account_owner.id" +
